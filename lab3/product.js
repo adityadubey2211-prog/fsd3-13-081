38b53030e6239d37d196e0e3acfcbcacab8c1e41
adityadubey2211-prog <adityadubey2211@gmail.com>
2026-09-22 13:44:58 +0530

    product.js

---
+++ lab3/product.js
@@ -0,0 +1,10 @@
+const products = [
+    {id:1, name:'marker', qty:100, price:15}
+    {id:2, name:'duster', qty:50, price:10}
+    
+]
+let nextid = 3;
+const getAllProducts = () => {
+    return products;
+    
+}
